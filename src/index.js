b2e7262d002fec54e336c09e0955439f198feec9
ciju <ciju.ch3rian@gmail.com>
2017-08-10 15:12:39 +0530

    Use react-snapshot
    Renders the static html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
